1d92648d88d5fe97b76dde46d8409af1d1a3c948
Imyourking <1556732178@qq.com>
2018-04-17 08:59:48 +0800

    添加

--- esthgrfbg/esthgrfbg/Form1.Designer.cs
+++ esthgrfbg/esthgrfbg/Form1.Designer.cs
@@ -217,6 +217,7 @@
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
